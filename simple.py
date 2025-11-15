ef6d3c39162664a1278ba7c1658b23e29e8470d7
Mingjin Lu <mlu2002@outlook.com>
2025-11-15 15:35:55 -0500

    1

--- simple.py
+++ simple.py
@@ -1,12 +1,32 @@
-from gpiozero import Servo
-from time import sleep
+import time
+import lgpio
 
-s = Servo(18)
+chip = lgpio.gpiochip_open(0)
 
-while True:
-    s.min()
-    sleep(1)
-    s.mid()
-    sleep(1)
-    s.max()
-    sleep(1)
+SERVO = 18   # use a hardware PWM pin: 12/13/18/19
+FREQ = 50    # servo frequency
+
+def set_angle(angle):
+    # MG90S recommended range 2.5 12.5
+    duty = 2.5 + (angle / 180) * 10  # percent
+    lgpio.tx_pwm(chip, SERVO, FREQ, duty)
+
+try:
+    while True:
+        print("Forward (simulate) ? rotate to 180")
+        set_angle(180)
+        time.sleep(2)
+
+        print("Stop ? hold at 90")
+        set_angle(90)
+        time.sleep(2)
+
+        print("Backward (simulate) ? rotate to 0")
+        set_angle(0)
+        time.sleep(2)
+
+except KeyboardInterrupt:
+    pass
+
+lgpio.gpio_free(chip, SERVO)
+lgpio.gpiochip_close(chip)
